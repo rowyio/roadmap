4157eb6f23b2ec7b720ffb7e47a201ac7b3217d2
iamanishroy <6275anishroy@gmail.com>
2023-02-13 10:57:12 +0530

    updated link

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = ({
 }) => {
   return (
     <div className="navbar bg-base-100 shadow-md px-2 md:mb-3 md:rounded-lg">
-      <Link to="/roadmap">
+      <Link to="https://www.rowy.io/?utm_source=roadmap">
         <div className="flex flex-col justify-start select-none rounded-lg px-4 py-1">
           <span className="text-2xl font-bold">Roadmap</span>
           <span className="text-xs">Powered by Rowy</span>
